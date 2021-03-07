fd217d52031a159add6619931b721552f74067b2
Shah Lav <developer17ls@gmail.com>
2021-03-08 00:36:12 +0530

    Removed safemodel

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,10 @@
 import * as mongoose from 'mongoose';
-import _ from 'lodash';
 
 export const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
 });
-
-ProductSchema.method({
-  safeModel() {
-    return _.omit(this.toObject(), ['__v']);
-  },
-})
 
 export interface Product extends mongoose.Document {
   id: string;
